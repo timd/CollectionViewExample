bb7f7b275b7b077bd4fd98d820a025785170f446
Tim Duckett <tim@charismaticmegafauna.co.uk>
2012-09-05 12:26:35 +0100

    Initial Commit

---
+++ CollectionViewExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CollectionViewExample
+//
+//  Created by Tim on 9/5/12.
+//  Copyright (c) 2012 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
